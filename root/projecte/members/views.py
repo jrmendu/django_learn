4b8e242260e1a04c17177191e7f625be1951493b
jr <jrmendu@gmail.com>
2023-01-13 16:57:40 +0100

    Primers passos per crear projecte Django

---
+++ root/projecte/members/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.template import loader
+
+	
+def members(request):
+    template = loader.get_template('prova.html')
+    return HttpResponse(template.render())
+
+
